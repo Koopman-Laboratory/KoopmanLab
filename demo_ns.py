ade8a710a45ce5a1730f90db5cbfa8e28ad41b77
Xiong Wei <118104436+XiongWeiTHU@users.noreply.github.com>
2023-01-02 16:26:27 +0800

    Update demo_ns.py

--- demo_ns.py
+++ demo_ns.py
@@ -6,14 +6,14 @@ torch.cuda.set_device(0)
 device = torch.device("cuda")
 # Loading Data
 path = "./data/ns_V1e-3_N5000_T50.mat"
-train_loader, test_loader = kp.data.navier_stokes(path, batch_size = 10, T_in = 10, T = 40, type = "1e-3", sub = 1)
+train_loader, test_loader = kp.data.navier_stokes(path, batch_size = 10, T_in = 10, T_out = 40, type = "1e-3", sub = 1)
 # Hyper parameters
-ep = 750
+ep = 1
 o = 32
 m = 16
 r = 8
 # Model
-koopman_model = kp.model.koopman(backbone = "KNO2d", autoencoder = "MLP", o = o, m = m, r = r, t_len = 10, device = device)
+koopman_model = kp.model.koopman(backbone = "KNO2d", autoencoder = "MLP", o = o, m = m, r = r, t_in = 10, device = device)
 koopman_model.compile()
 koopman_model.opt_init("Adam", lr = 0.005, step_size=100, gamma=0.5)
 koopman_model.train(epochs=ep, trainloader = train_loader, evalloader = test_loader)
