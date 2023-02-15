be459ad50ccb4f776ccae2e4d5aa3cbed772d1d1
Xiong Wei <118104436+XiongWeiTHU@users.noreply.github.com>
2023-02-15 09:58:37 +0800

    Update __init__.py

--- koopmanlab/__init__.py
+++ koopmanlab/__init__.py
@@ -6,3 +6,6 @@ from . import koopmanViT
 from . import model
 from . import func
 from .model import koopman, koopman_vit
+from .models import KNO1d,KNO2d
+from .models import encoder_mlp, decoder_mlp, encoder_conv1d, decoder_conv1d, encoder_conv2d, decoder_conv2d
+from .models import ViT
